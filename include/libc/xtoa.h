70adc608677fa809607f9d2a677f33067be15e80
copi143 <copi143@outlook.com>
2024-07-22 17:02:20 +0800

    完成 xtoa

--- include/libc/xtoa.h
+++ include/libc/xtoa.h
@@ -2,8 +2,10 @@
 #include <define.h>
 #include <type.h>
 
-extern char  *_plos_c_xtoa_buf;
-extern size_t _plos_c_xtoa_buf_len;
+#define _plos_c_xtoa_buf_len 32
+extern char _plos_c_xtoa_buf[_plos_c_xtoa_buf_len];
+
+// 这些函数将数字转换为字符串形式
 
 #define __toa(t, type)                                                                             \
   finline cstr t##toa(type n) {                                                                    \
@@ -58,17 +60,14 @@ _utoa(u64);
 
 //
 
-extern char _plos_lut_alnum_lower[62];
-extern char _plos_lut_alnum_upper[62];
-
-char _plos_lut_alnum_lower[62] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char _plos_lut_alnum_upper[62] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+extern const char _plos_lut_alnum_lower[62];
+extern const char _plos_lut_alnum_upper[62];
 
 #define __tostr_begin                                                                              \
   char *s = buf + len;                                                                             \
   *--s    = '\0';
 #define __tostr_begin_nlt                                                                          \
-  const char *nlt = _plos_lut_alnum_upper;                                                         \
+  cstr nlt = _plos_lut_alnum_upper;                                                                \
   if (b < 0) (b = -b, nlt = _plos_lut_alnum_lower);                                                \
   __tostr_begin
 
@@ -139,7 +138,7 @@ char _plos_lut_alnum_upper[62] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghij
     bool  neg = _n < 0;                                                                            \
     utype n   = neg ? -_n : _n;                                                                    \
     for (; n; n >>= 4)                                                                             \
-      *--s = __LUT(alnum)[n & 15];                                                                 \
+      *--s = _plos_lut_alnum_lower[n & 15];                                                        \
     if (neg) *--s = '-';                                                                           \
     return s;                                                                                      \
   }
@@ -148,7 +147,7 @@ char _plos_lut_alnum_upper[62] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghij
     __tostr_begin;                                                                                 \
     if (n == 0) return (*--s = '0', s);                                                            \
     for (; n; n >>= 4)                                                                             \
-      *--s = __LUT(alnum)[n & 15];                                                                 \
+      *--s = _plos_lut_alnum_lower[n & 15];                                                        \
     return s;                                                                                      \
   }
 
@@ -159,7 +158,7 @@ char _plos_lut_alnum_upper[62] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghij
     bool  neg = _n < 0;                                                                            \
     utype n   = neg ? -_n : _n;                                                                    \
     for (; n; n >>= 4)                                                                             \
-      *--s = __LUT(ALNUM)[n & 15];                                                                 \
+      *--s = _plos_lut_alnum_upper[n & 15];                                                        \
     if (neg) *--s = '-';                                                                           \
     return s;                                                                                      \
   }
@@ -168,7 +167,7 @@ char _plos_lut_alnum_upper[62] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghij
     __tostr_begin;                                                                                 \
     if (n == 0) return (*--s = '0', s);                                                            \
     for (; n; n >>= 4)                                                                             \
-      *--s = __LUT(ALNUM)[n & 15];                                                                 \
+      *--s = _plos_lut_alnum_upper[n & 15];                                                        \
     return s;                                                                                      \
   }
 
@@ -192,30 +191,17 @@ char _plos_lut_alnum_upper[62] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghij
     return s;                                                                                      \
   }
 
-#define _tostrb2(t, u)  __tostrb2(t, t##_t, u##_t)
-#define _utostrb2(t)    __utostrb2(t, t##_t)
-#define _tostrb8(t, u)  __tostrb8(t, t##_t, u##_t)
-#define _utostrb8(t)    __utostrb8(t, t##_t)
-#define _tostrb10(t, u) __tostrb10(t, t##_t, u##_t)
-#define _utostrb10(t)   __utostrb10(t, t##_t)
-#define _tostrb16(t, u) __tostrb16(t, t##_t, u##_t)
-#define _utostrb16(t)   __utostrb16(t, t##_t)
-#define _tostrB16(t, u) __tostrB16(t, t##_t, u##_t)
-#define _utostrB16(t)   __utostrB16(t, t##_t)
-#define _tostr(t, u)    __tostr(t, t##_t, u##_t)
-#define _utostr(t)      __utostr(t, t##_t)
-
 #define __(name)                                                                                   \
-  __##name(hh, char, unsigned char);                                                               \
-  __u##name(uhh, unsigned char);                                                                   \
-  __##name(h, short, unsigned short);                                                              \
-  __u##name(uh, unsigned short);                                                                   \
-  __##name(i, int, unsigned int);                                                                  \
-  __u##name(ui, unsigned int);                                                                     \
-  __##name(l, long int, unsigned long int);                                                        \
-  __u##name(ul, unsigned long int);                                                                \
-  __##name(ll, long long int, unsigned long long int);                                             \
-  __u##name(ull, unsigned long long int);
+  __##name(hh, char, uchar);                                                                       \
+  __u##name(uhh, uchar);                                                                           \
+  __##name(h, short, ushort);                                                                      \
+  __u##name(uh, ushort);                                                                           \
+  __##name(i, int, uint);                                                                          \
+  __u##name(ui, uint);                                                                             \
+  __##name(l, long, ulong);                                                                        \
+  __u##name(ul, ulong);                                                                            \
+  __##name(ll, llong, ullong);                                                                     \
+  __u##name(ull, ullong);
 
 __(tostrb2);
 __(tostrb8);
@@ -226,6 +212,19 @@ __(tostrB16);
 __(tostr);
 
 #undef __
+
+#define _tostrb2(t, u)  __tostrb2(t, t, u)
+#define _utostrb2(t)    __utostrb2(t, t)
+#define _tostrb8(t, u)  __tostrb8(t, t, u)
+#define _utostrb8(t)    __utostrb8(t, t)
+#define _tostrb10(t, u) __tostrb10(t, t, u)
+#define _utostrb10(t)   __utostrb10(t, t)
+#define _tostrb16(t, u) __tostrb16(t, t, u)
+#define _utostrb16(t)   __utostrb16(t, t)
+#define _tostrB16(t, u) __tostrB16(t, t, u)
+#define _utostrB16(t)   __utostrB16(t, t)
+#define _tostr(t, u)    __tostr(t, t, u)
+#define _utostr(t)      __utostr(t, t)
 
 #define __(name)                                                                                   \
   _##name(i8, u8);                                                                                 \
