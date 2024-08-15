cc69dfb83a6468350bdfc693f63892cf06d26803
copi143 <copi143@outlook.com>
2024-08-16 03:01:07 +0800

    加入一个空的efi程序

---
+++ src/boot/efi.c
@@ -0,0 +1,6 @@
+#include <define.h>
+
+ssize_t efi_main(void *ImageHandle, void *SystemTable) {
+  infinite_loop;
+  return 0;
+}
---
+++ src/boot/make-efi.sh
@@ -0,0 +1,7 @@
+objcopy --only-keep-debug kernel.bin kernel.sym
+objcopy --strip-debug kernel.bin
+
+rm -r plos-efi || true
+mkdir -p plos-efi/EFI/Boot
+
+cp boot.efi plos-efi/EFI/Boot/plos.efi
