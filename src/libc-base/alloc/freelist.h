bda8f77b49c63b26b64e64726973650a3b2d03a0
copi143 <copi143@outlook.com>
2024-11-09 22:53:31 +0800

    支持对齐内存的分配

--- src/libc-base/alloc/freelist.h
+++ src/libc-base/alloc/freelist.h
@@ -22,6 +22,13 @@ finline int freelists_size2id(size_t size) {
   return (32 - 9) - __builtin_clz(size) + 2;
 }
 
+/**
+ *\brief 将 freelist 中的内存块分离
+ *
+ *\param list     空闲链表
+ *\param ptr      要分离的内存块指针
+ *\return 分离后的空闲链表
+ */
 finline freelist_t freelist_detach(freelist_t list, freelist_t ptr) {
   if (list == ptr) return ptr->next;
   if (ptr->next) ptr->next->prev = ptr->prev;
@@ -35,7 +42,7 @@ finline freelist_t freelist_detach(freelist_t list, freelist_t ptr) {
  *\param lists    空闲链表组
  *\param id       空闲链表的 id
  *\param ptr      要分离的内存块指针
- *\return value
+ *\return 分离的内存块指针 (即传入的 ptr)
  */
 finline void *freelists_detach(freelists_t lists, int id, freelist_t ptr) {
   if (lists[id] == ptr) {
@@ -48,6 +55,13 @@ finline void *freelists_detach(freelists_t lists, int id, freelist_t ptr) {
   return ptr;
 }
 
+/**
+ *\brief 匹配并将内存从 freelist 中分离
+ *
+ *\param list_p   空闲链表指针
+ *\param size     要寻找内存的最小大小
+ *\return 找到的内存块指针，未找到为 null
+ */
 finline void *freelist_match(freelist_t *list_p, size_t size) {
   for (freelist_t list = *list_p; list != null; list = list->next) {
     size_t tgt_size = blk_size(list);
@@ -77,6 +91,52 @@ finline void *freelists_match(freelists_t lists, size_t size) {
   }
   return null;
 }
+
+#define aligned_size_of(ptr, align)                                                                \
+  ((ssize_t)blk_size(ptr) - (ssize_t)(PADDING_UP(ptr, align) - (size_t)(ptr)))
+
+/**
+ *\brief 匹配并将内存从 freelist 中分离
+ *       要求内存能对齐到 align 指定的大小且对齐后至少有 size 的大小
+ *
+ *\param list_p   空闲链表指针
+ *\param size     要寻找内存的最小大小
+ *\param align    对齐大小 (必须为 2 的幂) (必须大于等于 2 倍字长)
+ *\return 找到的内存块指针，未找到为 null
+ */
+finline void *freelist_aligned_match(freelist_t *list_p, size_t size, size_t align) {
+  for (freelist_t list = *list_p; list != null; list = list->next) {
+    ssize_t tgt_size = aligned_size_of(list, align);
+    if (tgt_size >= size) {
+      *list_p = freelist_detach(*list_p, list);
+      return list;
+    }
+  }
+  return null;
+}
+
+/**
+ *\brief 匹配并将内存从 freelist 中分离
+ *       要求内存能对齐到 align 指定的大小且对齐后至少有 size 的大小
+ *
+ *\param lists    空闲链表 (组)
+ *\param size     要寻找内存的最小大小
+ *\param align    对齐大小 (必须为 2 的幂) (必须大于等于 2 倍字长)
+ *\return 找到的内存块指针，未找到为 null
+ */
+finline void *freelists_aligned_match(freelists_t lists, size_t size, size_t align) {
+  int id = freelists_size2id(size);
+  if (id < 0) return null;
+  for (; id < FREELIST_NUM; id++) {
+    for (freelist_t list = lists[id]; list != null; list = list->next) {
+      ssize_t tgt_size = aligned_size_of(list, align);
+      if (tgt_size >= size) return freelists_detach(lists, id, list);
+    }
+  }
+  return null;
+}
+
+#undef aligned_size_of
 
 /**
  *\brief 将元素放到 freelist 中
